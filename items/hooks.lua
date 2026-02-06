a372d6bba7d6d1ade40a174c2e270982bb18705a
TheOneGoofAli <alilesha552@gmail.com>
2026-02-07 01:57:58 +0300

    1.12.3
    +1 Joker, +2 Decks, +6 Enhancements (3 normal and 3 Alloy)
    Anvil reworked again, felt it was still underwhelming.
    Albanian Virus reworked, no longer losing a run by itself.
    None Battery now has an effect when a battery is present.
    Smeltery and Prospect now show their respective badges below the main "Tarot" badge.
    GLTeapot is now stack-aware.
    Added Saturn stacking compatibility.
    Incantation restriction removed, but a warning will be shown.

--- items/hooks.lua
+++ items/hooks.lua
@@ -763,6 +763,18 @@ function Game:main_menu(ctx)
 			func = function() togabalatro.cryptidnotice() return true end
 		}))
 	end
+	-- PWX warning.
+	if next(SMODS.find_mod('jen')) then
+		G.E_MANAGER:add_event(Event({
+			func = function() togabalatro.pwxnotice() return true end
+		}))
+	end
+	-- Incantation w/o PWX.
+	if not next(SMODS.find_mod('jen')) and next(SMODS.find_mod('incantation')) then
+		G.E_MANAGER:add_event(Event({
+			func = function() togabalatro.incantationnotice() return true end
+		}))
+	end
 	return r
 end
 
@@ -854,6 +866,9 @@ function get_blind_amount(ante)
 	if deband[1] then
 		amt = amt*0.8
 	end
+	for k, v in pairs(G.playing_cards or {}) do
+		if SMODS.has_enhancement(v, 'm_toga_lead') and tonumber(v.ability.toga_blindredamt) then amt = amt*v.ability.toga_blindredamt end
+	end
 	return amt
 end
 
@@ -924,4 +939,12 @@ function Card:use_consumeable(area, copier)
 			end
 		end
 	end
+end
+
+sendInfoMessage("Hooking Full House evaluation...", "TOGAPack")
+local fullhouse = SMODS.PokerHands['Full House']
+local fullhouseeval = SMODS.PokerHands['Full House'].evaluate
+function fullhouse.evaluate(parts)
+	if next(SMODS.find_card('j_toga_achemoth')) then return not (#parts._2 < 2) and parts._all_pairs end
+	return fullhouseeval(parts)
 end
